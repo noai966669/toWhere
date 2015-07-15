5a1a43d1f077f4e50e2a275112bdde8dcc864c0a
apple <apple@appledeMacBook-Air.local>
2015-07-15 14:21:49 +0800

    Initial Commit

---
+++ Towhere/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Towhere
+//
+//  Created by apple on 15/7/15.
+//  Copyright (c) 2015年 elephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
